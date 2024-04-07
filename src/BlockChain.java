3feed0fe39ade53690a045f8094e2d03b9ce63a5
Nye <tenerell@grinnell.edu>
2024-04-07 18:03:24 -0500

    new code

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,3 +1,5 @@
+import java.security.NoSuchAlgorithmException;
+
 /**
  * INSERT MESSAGE HERE
  * 
@@ -7,35 +9,17 @@
  */
 public class BlockChain {
   
-  Block val;  
-
-  Block next;
-
   int size;
-
   Node arr;
-
-  public Node(Block val, Block next) {  
-      this.val = val;  
-      this.next = next;
-  }  
-
 //     BlockChain(int initial): creates a BlockChain that possess a single block the starts with the given initial amount. Note that to create this block, the prevHash field should be ignored when calculating the block’s own nonce and hash.
-  public BlockChain(int initial) {
-    arr = (new Block(0, initial, null), null);
+  public BlockChain(int initial) throws NoSuchAlgorithmException {
+    arr = new Node (new Block(0, initial, null), null);
     size = 1;
   }
 //     Block mine(int amount): mines a new candidate block to be added to the end of the chain. The returned Block should be valid to add onto this chain.
-  public Block mine(int amount)  {
-
-    Node mover = arr;
-
-    while (mover.next != null) {
-      mover = mover.next;
-    }
-    mover.next = Node(blk, null);
-
-    Block blk = new Block(size + 1, amount, blk.prevHash());
+  public Block mine(int amount) throws NoSuchAlgorithmException  {
+    
+    Block blk = new Block(size + 1, amount, getHash());
     return blk;
   }
 //     int getSize(): returns the size of the blockchain. Note that number of the blocks provides a convenient method for quickly determining the size of the chain.
@@ -58,7 +42,7 @@ public class BlockChain {
     }
 
     size++;
-    mover.next = Node(blk, null);
+    mover.next = new Node(blk, null);
   }
 //     boolean removeLast(): removes the last block from the chain, returning true. If the chain only contains a single block, then removeLast does nothing and returns false.
   public boolean removeLast() {
@@ -84,9 +68,9 @@ public class BlockChain {
   }
 //     boolean isValidBlockChain(): walks the blockchain and ensures that its blocks are consistent (the balances are legal) and valid (as in append).
 public boolean isValidBlockChain()  {
-  Node mover = arr.next();
-  while (mover != null) {
-    if (!(blk.getPrevHash().equals(getHash()))) {
+  Node mover = arr;
+  while (mover.next != null) {
+    if (!(mover.val.getPrevHash().equals(mover.next.val.getHash()))) {
       return true;
     }
     mover = mover.next;
@@ -96,18 +80,20 @@ public boolean isValidBlockChain()  {
 //     void printBalances(PrintWriter pen): prints Alexis’s and Blake’s respective balances in the form Alexis: <amt>, Blake: <amt> on a single line, e.g., Alexis: 300, Blake: 0.
   public void getBalance()  {
     int balance = 0;
+    Node mover = arr;
 
     while (mover.next != null) {
       balance += mover.val.getAmount();
     }
-    System.out.println("Alexis: " + amount + ", Blake" + amount*-1 + 300);
+    System.out.println("Alexis: " + balance + ", Blake" + balance*-1 + 300);
   }
 //     String toString(): returns a string representation of the BlockChain which is simply the string representation of each of its blocks, earliest to latest, one per line.
   public String toString() {
-    StringBuffer str = new StringBuffer(""); 
+    String str = ""; 
+  Node mover = arr;
 
     while (mover.next != null) {
-      str.append("Block " + mover.val.getNum + "(Amount: " + mover.val.getAmount + ", Nonce: " + mover.val.getNonce + ", prevHash: " + mover.val.getPrevHash + ", Hash: "  + mover.val.getHash + ")\n");
+      str += ("Block " + mover.val.getNum() + "(Amount: " + mover.val.getAmount() + ", Nonce: " + mover.val.getNonce() + ", prevHash: " + mover.val.getPrevHash() + ", Hash: "  + mover.val.getHash() + ")\n");
     }
     return str;
   }
